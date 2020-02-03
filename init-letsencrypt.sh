324805295c8ab2c3191ca9dcfb6d3c1add86bd0f
Lscheinman <40657768+Lscheinman@users.noreply.github.com>
2020-02-03 15:47:50 +0100

    Update init-letsencrypt.sh

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -4,10 +4,10 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(tsx.eastus.cloudapp.azure.com www.tsx.eastus.cloudapp.azure.com)
+domains=(intcitium.com www.intcitium.com)
 rsa_key_size=4096
 data_path="./data/certbot"
-email="dave@terraschwartz.com" # Adding a valid address is strongly recommended
+email="lscheinman77@gmail.com" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
